05fdcaab7b09910d60aba8e0c65bc39668842fac
EpicAaruTHEpro <69062625+EpicAaruTHEpro@users.noreply.github.com>
2020-12-13 21:05:30 -0500

    Created the basics

---
+++ Game Elements.md
@@ -0,0 +1,14 @@
+## Game Elements/Elements of Game Design
+
+1. Rules: The player has the elements of water, fire, and thunder to help with platforming. If you get hit you lose a heart. If you lose all 3 of your hearts you die, and must restart.
+2. Gameplay/Story: You run and jump over obstacles and enemies with the help of your magical elemental abilities: fire, thunder, and water. The story is that Azocado man is the last of his kind and he is making a rebellion to destroy the production of fruits and veggies. However, Gef needs his fruits and Veggies to live and so he goes on a quest to destroy Azocado man and bring back production of fruits and veggies. 
+3. Characters
+   1. PC- playable character(s): Gef
+   2. NPC- non playable characters: Azocado Man, Canopus, Sirius, Bombchu, King Kuff, Funky Dunky, Derpy, Toji
+4. Balance of Difficulty: The elements help to defeat the enemies.
+5. Adaptivity
+6. Goal or Objective: To defeat the enemies on your path and platform over the obstacles you face to reach the end of the game.
+7. Skill vs. Random Elements: The skill is being able to platform over obstacles and enemies well and use your elements.
+8. Feedback: Scoring system based on the enemies killed and their type. There is also music that accompanies the gameplay
+9. This game will be played on PC as it uses mulitple keyboard keys.
+10. This Game is made on Visual Code studio using the p5.play library.
--- sketch.js
+++ sketch.js
@@ -1,9 +1,137 @@
+//image variables for the characters
+var azocadoImg, bombchuImg, canopusImg, derpyImg, funkydunkyImg, gefImg, kingkuffImg, siriusImg, tojiImg, caveImg, groundImg, restartImg, canopusJailed, tojiJailed, siriusJailed;
+var gef, derpy, kingkuff, bombchu, funkydunky, azocado, canopus, sirius, toji;
+var ground, restart;
+var health = 3;
+var gameState = "PLAY";
+
+//preloads images
+function preload() {
+  azocadoImg = loadImage("characters/Azocado.png");
+  bombchuImg = loadImage("characters/Bombchu.png");
+  canopusImg = loadImage("characters/Canopus.png");
+  canopusJailed = loadImage("characters/CanopusJailed.png");
+  derpyImg = loadImage("characters/Derpy.png");
+  funkydunkyImg = loadImage("characters/FunkyDunky.png");
+  gefImg = loadImage("characters/Gef.png");
+  kingkuffImg = loadImage("characters/KingKuff.png");
+  siriusImg = loadImage("characters/sirius.png");
+  siriusJailed = loadImage("characters/siriusJailed.png");
+  tojiImg = loadImage("characters/toji.png");
+  tojiJailed = loadImage("characters/tojiJailed.png");
+  caveImg = loadImage("characters/cave.png");
+  groundImg = loadImage("characters/ground.png");
+  restartImg = loadImage("characters/restart.png");
+}
+
 function setup() {
   createCanvas(800,400);
-  createSprite(400, 200, 50, 50);
+
+  ground = createSprite(400,392, 400, 20);
+  ground.addImage(groundImg);
+  ground.x = camera.position.x;
+  gef = createSprite(400, 150, 50, 50);
+  gef.addImage(gefImg);
+  gef.scale =2;
+  derpy = createSprite(1000, 270, 50, 50);
+  derpy.addImage(derpyImg);
+  bombchu = createSprite(2000, 270, 50, 50);
+  bombchu.addImage(bombchuImg);
+  kingkuff = createSprite(3000, 280, 50, 50);
+  //kingkuff.addImage(kingkuffImg);
+  //kingkuff.scale = 0.2;
+  azocado = createSprite(4000, 280, 50, 50);
+  azocado.addImage(azocadoImg);
+  canopus = createSprite(2100, 70, 50, 50);
+  canopus.addImage(canopusJailed);
+  toji = createSprite(1100, 70, 50, 50);
+  toji.addImage(tojiJailed);
+  sirius = createSprite(3100, 70, 50, 50);
+  sirius.addImage(siriusJailed);
+  restart = createSprite(camera.position.x+400, 200, 50, 50);
+  restart.addImage(restartImg);
+  restart.scale = 2;
+  restart.visible = false;
 }
 
 function draw() {
-  background(255,255,255);  
+  background(caveImg);
+  camera.position.x = gef.x;
+  ground.x = camera.position.x;
+
+  if (ground.x < camera.position.x-400) {
+    ground.x = camera.position.x;
+  }
+
+  if (gameState === "PLAY") {
+    if (keyDown("d")) {
+      gef.x+=5;
+    }
+
+    if (keyDown("a")) {
+      gef.x-=5;
+    }
+
+    gef.velocityY +=1;
+
+
+    if (keyDown("space") && gef.y > 250) {
+      gef.velocityY = -10;
+    }
+
+    if (gef.isTouching(derpy) && gef.y <= 260) {
+      toji.addImage(tojiImg);
+      console.log(derpy.x);
+      //derpy.destroy();
+    }
+
+    else if (gef.isTouching(derpy) && gef.y > 260){
+      health-=1;
+    }
+
+    if (gef.isTouching(kingkuff) && gef.y <= 260) {
+      sirius.addImage(siriusImg);
+      kingkuff.destroy();
+    }
+
+    else if (gef.isTouching(kingkuff) && gef.y > 260){
+      health-=1;
+    }
+
+    if (gef.isTouching(bombchu) && gef.y <= 260) {
+      canopus.addImage(canopusImg);
+      bombchu.destroy();
+    }
+
+    else if (gef.isTouching(bombchu) && gef.y > 260){
+      health-=1;
+    }
+
+    if (health === 0) {
+      gameState = "END";
+    }
+  }
+
+  else if (gameState === "END") {
+    gef.x = 400;
+    restart.visible = true;
+  }
+
+  if (mousePressedOver(restart) && gameState === "END") {
+    
+    health = 3;
+    toji.addImage(tojiJailed);
+    canopus.addImage(canopusJailed);
+    sirius.addImage(siriusJailed);
+    gameState = "PLAY";
+    restart.visible = false;
+  }
+
+  gef.collide(ground);
+  derpy.collide(ground);
+  bombchu.collide(ground);
+  kingkuff.collide(ground);
+  azocado.collide(ground);
   drawSprites();
+  //console.log(health);
 }
